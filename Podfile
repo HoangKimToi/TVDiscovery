1ca36569ff01116a2da7c2377b44cce2945f4064
Hoang Kim Toi <hoang.work99@gmail.com>
2024-08-30 15:08:19 +0700

    Init

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TVTEST' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'CocoaAsyncSocket'
+#  pod 'Weak', '~> 1.0.0'
+  # Pods for TVTEST
+
+end
--- TVTEST/ViewController.swift
+++ TVTEST/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let discovery: SSDPDiscovery = SSDPDiscovery.defaultDiscovery
+    fileprivate var session: SSDPDiscoverySession?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func startScan(sender: AnyObject) {
+//        let zonePlayerTarget = SSDPSearchTarget.deviceType(schema: SSDPSearchTarget.upnpOrgSchema, deviceType: "Samsung", version: 1)
+        let request = SSDPMSearchRequest(delegate: self, searchTarget: SSDPSearchTarget.all)
+        
+        // Start a discovery session for the request and timeout after 10 seconds of searching.
+        self.session = try! discovery.startDiscovery(request: request, timeout: 10.0)
+    }
+    
+    @IBAction func stopScan(sender: AnyObject) {
+        self.session?.close()
+        self.session = nil
+    }
+    
+    
 }
 
+extension ViewController: SSDPDiscoveryDelegate {
+    
+    public func discoveredDevice(response: SSDPMSearchResponse, session: SSDPDiscoverySession) {
+        print("Found device \(response)\n")
+    }
+    
+    public func discoveredService(response: SSDPMSearchResponse, session: SSDPDiscoverySession) {
+    }
+    
+    public func closedSession(_ session: SSDPDiscoverySession) {
+        print("Session closed\n")
+    }
+    
+}
